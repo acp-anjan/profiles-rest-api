14e918014203bf7e57e4cc7679ad36fd12cf7b8e
acpanjan <paudelanjanchandra@gmail.com>
2018-05-23 15:03:59 +0545

    addeded

---
+++ src/profiles_project/profile_api/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url,include
+
+from rest_framework.routers import DefaultRouter
+
+
+ 
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewset)
+router.register('login', views.LoginViewSet, base_name='login')
+
+urlpatterns = [
+    url(r'^hello-view/',views.HelloApiView.as_view()),
+    url(r'', include(router.urls)),
+    
+
+]
