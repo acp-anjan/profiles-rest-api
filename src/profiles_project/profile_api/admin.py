47662c0e4f6cf7060af9d499bf666813c5b072aa
acpanjan <paudelanjanchandra@gmail.com>
2018-05-23 15:49:11 +0545

    added profilefeed model migrate and added in django admin

--- src/profiles_project/profile_api/admin.py
+++ src/profiles_project/profile_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile, UserProfileManager
+from .models import UserProfile, UserProfileManager, ProfileFeedItem
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
